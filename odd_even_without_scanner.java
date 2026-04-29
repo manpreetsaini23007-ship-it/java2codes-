45abb69d7824a19d7a547b1e4f74f1d7a18c00df
Manpreet Singh <manpreetsaini23007>
2026-04-29 11:19:54 +0530

    odd even without scanner

---
+++ odd_even_without_scanner.java
@@ -0,0 +1,11 @@
+public class odd_even_without_scanner {
+    public static void main(String [] args){
+        int num = 10;
+        if(num %2 == 0 ){
+            System.out.print("the number " + num + " is even ");
+        }else{
+            System.out.print("the number " + num + " is odd ");
+        }
+    }
+}
+
