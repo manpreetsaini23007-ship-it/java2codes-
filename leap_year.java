60a53f1fdc656ea32f947cd297a3411f6c5c1abe
Manpreet Singh <manpreetsaini23007>
2026-04-29 11:18:33 +0530

    leap year

---
+++ leap_year.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class leap_year {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("enter a year: ");
+        int year = sc.nextInt();
+
+        if(year %4 == 0 ){
+            System.out.print( year+ " is a leap year");
+        }else if(year %400 ==0){
+            System.out.print( year+ " is a leap year");
+        }else if( year %100 != 0){
+            System.out.print( year+ " is not a leap year");
+        }else{
+            System.out.print( year+ " is not a leap year");
+        }
+        sc.close();
+    }
+}
