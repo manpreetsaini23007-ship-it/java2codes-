fa312a2a4ac68e7cc75fcfb0461eaa1cb2626015
Manpreet Singh <manpreetsaini23007>
2026-05-17 22:23:56 +0530

    sort array

---
+++ SortArray.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr={5,2,8,1};
+        Arrays.sort(arr);
+        for(int x:arr) System.out.print(x+" ");
+    }
+}
